322fec55b1cf0b9bdf101f7c645f3677d9646b18
yangle <yangle@luojilab.com>
2022-05-06 10:07:59 +0800

    first window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+// OpenGL是一个规范, 不同驱动开发商有不同的实现, 一个函数在不同的驱动里的位置不同, glad可以找到各自的位置, http://maxyoung.fun/blog/OpenGL%E7%8E%AF%E5%A2%83%E9%85%8D%E7%BD%AE.html#glad
+#include <glad/glad.h>
 // GLFW提供渲染的基本接口, 例如创建窗口、上下文、接受用户输入等, http://maxyoung.fun/blog/OpenGL%E7%8E%AF%E5%A2%83%E9%85%8D%E7%BD%AE.html#glfw
 #include <GLFW/glfw3.h>
-// OpenGL是一个规范, 不同驱动开发商有不同的实现, 一个函数在不同的驱动里的位置不同, glad可以找到各自的位置, http://maxyoung.fun/blog/OpenGL%E7%8E%AF%E5%A2%83%E9%85%8D%E7%BD%AE.html#glad
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
@@ -16,5 +26,60 @@ int main()
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
+  // 创建窗口对象, 参数分别是长、宽、名称, 后面两个参数暂时忽略
+  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  // 设置这个窗口为上下文
+  glfwMakeContextCurrent(window);
+  // 回调函数, 注册这个函数，告诉GLFW我们希望每当窗口调整大小的时候调用这个函数
+  // 这个函数将窗口大小和视口大小保持一致
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // 调用OpenGL函数之前需要初始化glad2
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // 保持窗口打开, 接受用户输入, 不断绘制
+  while (!glfwWindowShouldClose(window))
+  {
+    // 接受键盘输入
+    processInput(window);
+
+    // 渲染指令
+    // 清空颜色缓冲并填充为深蓝绿色
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // 将缓冲区的像素颜色值绘制到窗口
+    glfwSwapBuffers(window);
+    // 检查有没有触发事件
+    glfwPollEvents();
+  }
+
+  // 释放资源
+  glfwTerminate();
   return 0;
+}
+
+// 回调函数, 窗口大小改变时, 创建渲染窗口
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  // 渲染窗口的大小, glViewport函数前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度（像素）
+  // 图形学里的viewport视口
+  glViewport(0, 0, width, height);
+}
+
+// 接受ESC键退出
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
